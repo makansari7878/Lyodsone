2b691b12f3f7acaac896d5aaea4d2e664e1d1fab
kaleem <makanari.android@gmail.com>
2025-01-06 12:48:24 +0530

    mmain act

--- app/src/main/java/com/example/lyodsappone/MainActivity.kt
+++ app/src/main/java/com/example/lyodsappone/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+// Testing
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
